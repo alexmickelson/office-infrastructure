7bb43d38b4131153f4871c9e509e150319289963
Alex Mickelson <alexmickelson96@gmail.com>
2025-07-03 11:37:01 -0600

    update restore script

--- tailscale/restore.sh
+++ tailscale/restore.sh
@@ -2,10 +2,10 @@
 #! nix-shell -i bash -p bash
 
 HOSTS=(
-  # "144.17.92.11" 
-  # "144.17.92.12" 
-  "144.17.92.13" 
-  # "144.17.92.14" 
+  # "144.17.92.11"
+  # "144.17.92.12"
+  # "144.17.92.13"
+  # "144.17.92.14"
   # "144.17.92.15"
 )
 
@@ -20,20 +20,25 @@ for HOST in "${HOSTS[@]}"; do
   echo "[INFO] Restoring Tailscale backup to $TARGET..."
 
   echo "[INFO] Stopping and starting tailscaled on $TARGET..."
-  ssh "$TARGET" "sudo -S systemctl start tailscaled && sudo -S systemctl stop tailscaled"
+  ssh "$TARGET" \
+    "sudo -S bash -c 'systemctl start tailscaled && \
+      systemctl stop tailscaled'"
 
   echo "[INFO] Copying backup to $TARGET..."
+  ssh "$TARGET" \
+    "rm -rf ~/tailscale_restore \
+      && mkdir -p ~/tailscale_restore"
 
-  ssh "$TARGET" "rm -rf ~/tailscale_restore && mkdir -p ~/tailscale_restore"
   scp -r "$BACKUP_DIR/tailscale_backup/." "$TARGET:~/tailscale_restore/"
 
   echo "[INFO] Restoring backup to $REMOTE_DIR on $TARGET..."
-  ssh "$TARGET" "sudo -S rm -rf $REMOTE_DIR && \
-      sudo -S mv ~/tailscale_restore $REMOTE_DIR && \
-      sudo -S chown -R _tailscale:_tailscale $REMOTE_DIR"
-
-  echo "[INFO] Restarting tailscaled on $TARGET..."
-  ssh "$TARGET" "sudo -S systemctl start tailscaled"
+  ssh "$TARGET" \
+    "sudo -S bash -c '\
+      rm -rf $REMOTE_DIR && \
+      mv /home/alex/tailscale_restore $REMOTE_DIR && \
+      chown -R root:root $REMOTE_DIR && \
+      systemctl start tailscaled\
+    '"
 
   echo "[SUCCESS] Restore to $HOST completed."
 done
